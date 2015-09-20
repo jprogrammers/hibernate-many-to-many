775bf0ac1237e256a8ffead397dc4fd02a63ff86
en20 <vahid.forghani@gmail.com>
2015-09-20 14:52:35 +0430

    hibernate many to many relationship example

---
+++ src/main/java/com/jprogrammers/model/Course.java
@@ -0,0 +1,46 @@
+package com.jprogrammers.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by jprogrammers.
+ */
+@Entity
+@Table(name = "course")
+public class Course {
+
+    long id;
+    String name;
+    List<User> users;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(mappedBy = "courses")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
